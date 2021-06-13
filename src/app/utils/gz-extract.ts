bb9e79d5f0bfe5be304f1b21ae0f8dfcec57fbf3
lulu2002 <lose9749@gmail.com>
2021-06-13 14:13:07 +0800

    解決無法讀取某些 tar gz 的問題
    tar gz 如果是使用 gnu 技術壓縮，且 path > 100 個字元時，便會將檔名分割出來變成另一份 「專門存取檔名」的 @LongLink 文件，因此我自己 fork 了 browser-untar 這個專案，並將它改造成可以讀取 gnu targz 的專案，目前總算可以直接把下載下來的 gz 直接放上去分析了！

--- src/app/utils/gz-extract.ts
+++ src/app/utils/gz-extract.ts
@@ -1,13 +1,15 @@
+import * as browserUntar from 'browser-untar/dist/browser-untar.js';
 import * as pako from 'pako/index.js';
-import * as untar from 'js-untar';
 import {UnZippedFile} from './un-zipped-file';
+
+const untar = browserUntar.default.instance();
 
 export class GzExtract {
   public static extract(tarGzFile: File): Promise<UnZippedFile[]> {
     return tarGzFile.arrayBuffer()
       .then(pako.inflate)
       .then(arr => (arr as Uint8Array).buffer)
-      .then(untar)
+      .then(value => untar.untar(value))
       .then(value => value as unknown as UnZippedFile[]);
   }
 }
