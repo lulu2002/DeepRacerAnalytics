234a5dcbe2f15b5458d6e949391c5c91fbb50eda
lulu2002 <lose9749@gmail.com>
2021-07-03 14:43:58 +0800

    完成 Track 資料分離功能，先備份

--- src/app/objects/tracks/china-sudu.ts
+++ src/app/objects/tracks/china-sudu.ts
@@ -1,21 +1,22 @@
 import {Track} from './track';
 import {Coords} from '../coords';
+import * as NpyJs from 'npyjs/index.js';
 
-export class EmptyTrack extends Track {
+export class ChinaSuduTrack extends Track {
 
-    constructor() {
-        super('China_track');
-    }
+  constructor() {
+    super('China_track');
+  }
 
-    protected registerInsideBorders(): Coords[] {
-        return [];
-    }
+  protected registerInsideBorders(): Coords[] {
+    return [];
+  }
 
-    protected registerOutsideBorders(): Coords[] {
-        return [];
-    }
+  protected registerOutsideBorders(): Coords[] {
+    return [];
+  }
 
-    protected registerHumanBestRoute(): Coords[] {
-        return [];
-    }
+  protected registerHumanBestRoute(): Coords[] {
+    return [];
+  }
 }
